3f68ce0f6325d4cf14dabb66a98555673956ab00
Ricardo <rick_ferry1@hotmail.com>
2023-12-20 22:33:58 -0300

    Again

---
+++ doctrine/bin/doctrine.php
@@ -0,0 +1,15 @@
+<?php
+
+use Doctrine\ORM\Exception\MissingMappingDriverImplementation;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+use Ferry\Doctrine\Helper\EntityManagerCreator;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+try {
+    $entityManager = EntityManagerCreator::getEntityManager();
+    ConsoleRunner::run(new SingleManagerProvider($entityManager));
+} catch (\Doctrine\DBAL\Exception|MissingMappingDriverImplementation $e) {
+    echo $e->getMessage();
+}
