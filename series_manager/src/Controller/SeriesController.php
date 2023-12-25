efaa95fb6e16f58276c4868224e1b537d2231d03
Ricardo <rick_ferry1@hotmail.com>
2023-12-25 15:29:28 -0300

    Feature de cadastro de serie com Symfony, Doctrine, ...

--- series_manager/src/Controller/SeriesController.php
+++ series_manager/src/Controller/SeriesController.php
@@ -2,30 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SeriesController extends AbstractController
 {
+    public function __construct(private readonly SerieRepository $repository)
+    {
+    }
+
     #[Route('/series', methods: ['GET'])]
     public function index(): Response
     {
-        $series = [
-            'Loki',
-            'Suits',
-            'Super Girl',
-            'The Flash',
-            'The Walking Dead'
-        ];
+        $series = $this->repository->findAll();
         return $this->render('series/index.html.twig', [
             'series' => $series,
         ]);
     }
 
     #[Route('/series/create', methods: ['GET'])]
-    public function addSeries(): Response
+    public function serieForm(): Response
     {
         return $this->render('series/form.html.twig');
+    }
+
+    #[Route('/series/create', methods: ['POST'])]
+    public function addSerie(Request $request): Response
+    {
+        $this->repository->add(
+            new Serie($request->request->get('name')),
+            true
+        );
+        return new RedirectResponse('/series');
     }
 }
---
+++ series_manager/src/Entity/Serie.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SerieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SerieRepository::class)]
+class Serie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id ;
+
+    #[ORM\Column]
+    private string $name;
+
+    /**
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
