e7f8451b023383756645c8d10b5b26abc0227062
Ricardo <rick_ferry1@hotmail.com>
2024-01-06 00:08:22 -0300

    All CRUD ready!

---
+++ books_api/migrations/Version20240106030044.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240106030044 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__book AS SELECT id, titulo, isbn, created_at, updated_at FROM book');
+        $this->addSql('DROP TABLE book');
+        $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, isbn VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , updated_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        )');
+        $this->addSql('INSERT INTO book (id, title, isbn, created_at, updated_at) SELECT id, titulo, isbn, created_at, updated_at FROM __temp__book');
+        $this->addSql('DROP TABLE __temp__book');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__book AS SELECT id, title, isbn, created_at, updated_at FROM book');
+        $this->addSql('DROP TABLE book');
+        $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, titulo VARCHAR(255) NOT NULL, isbn VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , updated_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        )');
+        $this->addSql('INSERT INTO book (id, titulo, isbn, created_at, updated_at) SELECT id, title, isbn, created_at, updated_at FROM __temp__book');
+        $this->addSql('DROP TABLE __temp__book');
+    }
+}
