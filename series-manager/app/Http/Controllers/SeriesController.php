35a72ca8236ea3039dd8482a4465fbdf9b83ae40
Ricardo <rick_ferry1@hotmail.com>
2024-01-07 01:27:51 -0300

    Eloquent with sqlite

--- series-manager/app/Http/Controllers/SeriesController.php
+++ series-manager/app/Http/Controllers/SeriesController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Series;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Request;
 
 class SeriesController extends Controller
 {
     public function index(): string
     {
-        $series = [
-            'Grey\'s Anatomy',
-            'Lost',
-            'Agents of SHIELD'
-        ];
+        $series = Series::all();
 
         return view('series.index')->with('series', $series);
     }
@@ -22,5 +23,17 @@ class SeriesController extends Controller
     public function create(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('series.create');
+    }
+
+    public function store(Request $request):
+            \Illuminate\Contracts\Foundation\Application|RedirectResponse|Redirector|Application
+    {
+        $seriesName = $request->request->get('name');
+
+        $series = new Series();
+        $series->name = $seriesName;
+        $series->save();
+
+        return redirect('/series');
     }
 }
--- series-manager/resources/views/series/create.blade.php
+++ series-manager/resources/views/series/create.blade.php
@@ -1,8 +1,9 @@
 <x-layout title="Nova Série:">
-<form action="" method="post">
+<form action="/series/save" method="post">
+    @csrf
     <div class="mb-3">
-        <label class="form-label" for="nome">Nome:</label>
-        <input class="form-control" type="text" name="nome" id="nome">
+        <label class="form-label" for="name">Nome:</label>
+        <input class="form-control" type="text" name="name" id="name">
     </div>
     <button class="btn btn-primary">Adicionar</button>
 </form>
